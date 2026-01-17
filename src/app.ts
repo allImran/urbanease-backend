8759936e5fe62e0879af0ee7546cd09088350dfb
allImran <alemrandev@gmail.com>
2026-01-17 15:01:53 +0600

    check

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,33 @@
-import express from 'express'
-import cors from 'cors'
-import router from './routes'
-import { errorMiddleware } from './middlewares/error.middleware'
+// import express from 'express'
+// import cors from 'cors'
+// import router from './routes'
+// import { errorMiddleware } from './middlewares/error.middleware'
 
-const app = express()
+// const app = express()
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
-app.use('/api', router)
-app.use(errorMiddleware)
+// app.use('/api', router)
+// app.use(errorMiddleware)
 
 
-// if (process.env.NODE_ENV !== 'production') {
-//   app.listen(3000, () => {
-//     console.log('Server running on http://localhost:3000')
-//   })
-// }
+// // if (process.env.NODE_ENV !== 'production') {
+// //   app.listen(3000, () => {
+// //     console.log('Server running on http://localhost:3000')
+// //   })
+// // }
 
-export default app
+// export default app
+
+// Use "type: module" in package.json to use ES modules
+import express from 'express';
+const app = express();
+ 
+// Define your routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+ 
+// Export the Express app
+export default app;
